be689d7d8c29bfb692b774c33e3cd4158ee8fd13
Kapil Sharma <kapil@kapilsharma.info>
2019-03-27 01:04:11 +0530

    Day 9: Completed header menu and footer. Slight problems on footer.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,20 +24,24 @@
             <div class="collapse navbar-collapse" id="PHPRebootNavBar">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#">Magazine <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="#">Articles</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
+                        <a class="nav-link" href="#">User Groups</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Meet-ups</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About</a>
                         <div class="dropdown-menu" aria-labelledby="dropdown03">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
+                            <a class="dropdown-item" href="#">What is PHP Reboot</a>
+                            <a class="dropdown-item" href="#">What you can do at PHP Reboot</a>
+                            <a class="dropdown-item" href="#">Developers</a>
+                            <a class="dropdown-item" href="#">Maintainers</a>
                         </div>
                     </li>
                 </ul>
@@ -276,6 +280,39 @@
                 </div>
             </div>
         </div>
+
+        <footer id="footer">
+            <div id="copyrights">
+                <div class="container clearfix">
+                    <div class="col_half">
+                        Copyrights &copy; 2019 All Rights Reserved by PHP Reboot.<br>
+                        <div class="copyright-links"><a href="#">Terms of Use</a> / <a href="#">Privacy Policy</a></div>
+                    </div>
+                    <div class="col_half col_last text-right">
+                        <div class="float-right clearfix">
+                            <a href="#" class="social-icon si-small si-borderless si-facebook">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                            <a href="#" class="social-icon si-small si-borderless si-twitter">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                            <a href="#" class="social-icon si-small si-borderless si-gplus">
+                                <i class="fab fa-github-alt"></i>
+                            </a>
+                            <a href="#" class="social-icon si-small si-borderless si-pinterest">
+                                <i class="fab fa-instagram"></i>
+                            </a>
+                            <a href="#" class="social-icon si-small si-borderless si-pinterest">
+                                <i class="fab fa-youtube"></i>
+                            </a>
+                        </div>
+                        <div class="clear"></div>
+                        <i class="far fa-envelope"></i>
+                        <span>info@phpreboot.com</span>
+                    </div>
+                </div>
+            </div>
+        </footer>
 
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
